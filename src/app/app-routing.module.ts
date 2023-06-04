8a23224baa60e32b0c920b4cb8cb3f3edf2fa8d5
franciapareja <francia.pareja@gmail.com>
2023-06-04 16:04:52 -0500

    se incluye codigo pendiente en app-routing.modules.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { FormulariohabitacionComponent } from './formulariohabitacion/formulariohabitacion.component';
+import { FormularioreservaComponent } from './formularioreserva/formularioreserva.component';
 
 
 const routes: Routes = [
   {path:"",component: HomeComponent, pathMatch: "full"},
   {path:"registrohabitacion", component: FormulariohabitacionComponent}, 
+  {path: "registroreservas", component: FormularioreservaComponent}
 ];
 
 @NgModule({
